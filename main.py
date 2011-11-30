ff2e3cf6cf4ab9f2217b85aeef9c99a79b5c3de5
Jason Hsu <github@swiftlinux.com>
2011-11-30 12:23:23 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Proper header for a Bash script.
+
+# Check for root user login
+if [ ! $( id -u ) -eq 0 ]; then
+	echo "You must be root to run this script."
+	echo "Please enter su before running this script again."
+	exit 2
+fi
+
+# Get your username (not root)
+UNAME=$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)
+DIR_DEVELOP=''
+
+# The remastering process uses chroot mode.
+# Check to see if this script is operating in chroot mode.
+# /home/mint directory only exists in chroot mode
+IS_CHROOT=0
+if [ -d "/home/mint" ]; then
+	IS_CHROOT=1 # in chroot mode
+	DIR_DEVELOP=/usr/local/bin/develop 
+else
+	DIR_DEVELOP=/home/$UNAME/develop 
+fi
+
+# Everything up to this point is common to the script shared-*.sh and all Bash scripts called by shared-*.sh
+# ==========================================================================================================
+
+print '============================'
+print 'BEGIN THE LINUX MINT UPDATES'
+
+print 'COMPLETED THE LINUX MINT UPDATES'
+print '================================'
